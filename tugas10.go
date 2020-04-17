816fb5e7a70ca218d4746863ae1d45f557a44cbd
hajidalakhtar <hajidalakhtar@gmail.com>
2020-04-17 09:20:07 +0700

    Add files via upload

---
+++ tugas10.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Nama Saya Muhammad Hajid Al Akhtar")
+	time.Sleep(time.Second * 5)
+	fmt.Println("Hobi saya Baca quora")
+	time.Sleep(time.Second * 5)
+	fmt.Println("tgl lahir saya 15 febuari 2002")
+	time.Sleep(time.Second * 5)
+	fmt.Println("Abisss")
+}
